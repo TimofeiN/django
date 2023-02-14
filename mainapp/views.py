b0594ef0f6fc376b9fbccc40af15b2eddd6bcb4b
Timofei <timofejnazarov@gmail.com>
2023-02-14 11:16:36 +0400

    lesson_3 Template's filters

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.views.generic import TemplateView
 
 
@@ -14,11 +16,11 @@ class NewsPageView(TemplateView):
         context = super().get_context_data(**kwargs)
         # Create your own data
         context["news_title"] = "Громкий новостной заголовок"
-        context[
-            "news_preview"
-        ] = "Предварительное описание, которое заинтересует каждого"
+        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
         context["range"] = range(5)
+        context["datetime_obj"] = datetime.now()
         return context
+
 
 
 class CoursesPageView(TemplateView):
